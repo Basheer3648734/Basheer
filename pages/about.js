fa7ba0dfb90a800b70d3e75c59236e6bb9fa22f1
Basheer3648734 <bk368734@gmail.com>
2022-01-26 22:26:32 +0530

    🐛 FIX: fixed vercel build bug

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import NavBar from "../components/NavBar";
 import Head from "next/head";
 export default function about() {
