f010d91e18112e9b120f5f50ef904bbcda1e4c99
Basheer3648734 <bk368734@gmail.com>
2022-01-27 13:38:28 +0530

    📦 NEW: completed projects route

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-export default function ProjectCard() {
+export default function ProjectCard({
+  name,
+  createdAt,
+  technologies,
+  id,
+  image,
+}) {
   // image, title, visit site, created-date
   return (
     <div className="bg-[white] w-[auto] shadow-lg shadow-gray-500/50 inline-block mx-[30px] my-[15px]">
@@ -9,35 +15,29 @@ export default function ProjectCard() {
       </div>
       <div className="px-[10px] py-[5px]">
         <h2 className="text-[1.25rem] font-bold inline-block md:text-[1.3rem]">
-          Book marker
+          {name}
         </h2>
         <p className="inline-block float-right text-[0.6rem] leading-5 font-medium text-[silver]">
-          01/02/2022
+          {createdAt}
         </p>
         <p className="underline text-[0.78rem] decoration-solid mt-[5px] text-[gray] md:text-[0.9rem]">
           Technologies used
         </p>
         <ul className="my-[10px] flex text-[0.8rem] md:text-[0.9rem] justify-around">
-          <li>
-            <Link href="#">
-              <a className="hover:underline hover:decoration-solid">
-                Tailwindcss
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="#">
-              <a className="hover:underline hover:decoration-solid">React.js</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="#">
-              <a className="hover:underline hover:decoration-solid">Firebase</a>
-            </Link>
-          </li>
+          {technologies.map((technology, index) => {
+            return (
+              <li key={index}>
+                <Link href={technology.url}>
+                  <a className="hover:underline hover:decoration-solid">
+                    {technology.name}
+                  </a>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
-        <Link href="https://bookmarker-ba651.web.app/login">
-          <a className="text-brand font-medium text-[1rem] mb-[10px] block text-center hover:underline hover:decoration-solid md:text-[1.2rem]">
+        <Link href={`/project/${id}`}>
+          <a className="text-black font-medium text-[1rem] mb-[10px] block text-center underline  md:text-[1.2rem] hover:text-[#1572A1]">
             Visit &#8594;
           </a>
         </Link>
--- pages/projects.js
+++ pages/projects.js
@@ -1,23 +1,37 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
 import NavBar from "../components/NavBar";
-
+import fs from "fs";
+import path from "path";
 import ProjectCard from "../components/ProjectCard";
-export default function projects() {
+export default function projects({ projects }) {
   return (
-    <div className="">
+    <div>
       <Head>
         <title>Basheer: Projects Page</title>
       </Head>
       <Layout>
         <NavBar page="projects" />
+        <div className=" font-bold text-[2rem]  p-[10px] mb-[10px] flex justify-center">
+          <h1 className="projects_highlighter">Projects 💼</h1>
+        </div>
         <div className="w-[100%] flex flex-wrap justify-evenly items-center flex-1 basis-[auto]">
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+          {projects.map((project) => {
+            return <ProjectCard {...project} key={project.id} />;
+          })}
         </div>
       </Layout>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const projectsDBFile = path.join(__dirname, "../../../data/projects.json");
+  const data = JSON.parse(fs.readFileSync(projectsDBFile));
+  console.log(data);
+  return {
+    props: {
+      projects: data,
+    },
+  };
 }
