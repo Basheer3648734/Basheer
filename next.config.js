e2eeb381b23d8dfafba16f8ba97964115bc73c62
Basheer <bk3648734@gmail.com>
2022-01-28 23:43:53 +0530

    📦 NEW: completed refactoring UI (#8)
    Co-authored-by: Basheer3648734 <bk368734@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   webpack5: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+    config.resolve.fallback = { fs: false, process: false };
 
     return config;
   },
